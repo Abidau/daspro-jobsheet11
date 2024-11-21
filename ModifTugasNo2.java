4761e0b5e99ee2e80d79f5de22b56d8442905ee0
NaufalAbidAurizky <naufalabidau@gmail.com>
2024-11-21 11:38:33 +0700

    Jobsheet11

--- ModifTugasNo2.java
+++ ModifTugasNo2.java
@@ -11,33 +11,33 @@ public class ModifTugasNo2 {
         int jumlahTugas = sc.nextInt();
 
         int[][] scores = new int[jumlahMahasiswa][jumlahTugas];
-        String[] studentNames = new String[jumlahMahasiswa];
+        String[] mahasiswa = new String[jumlahMahasiswa];
 
         System.out.println("\nMasukkan data mahasiswa:");
         for (int i = 0; i < jumlahMahasiswa; i++) {
             System.out.print("Nama Mahasiswa " + (i + 1) + ": ");
             sc.nextLine(); 
-            studentNames[i] = sc.nextLine();
+            mahasiswa[i] = sc.nextLine();
 
             for (int j = 0; j < jumlahTugas; j++) {
-                System.out.print("Nilai Tugas " + (j + 1) + " untuk " + studentNames[i] + ": ");
+                System.out.print("Nilai Tugas " + (j + 1) + " untuk " + mahasiswa[i] + ": ");
                 scores[i][j] = sc.nextInt();
             }
         }
 
-        tampilkanNilaiMahasiswa(studentNames, scores);
+        tampilkanNilaiMahasiswa(mahasiswa, scores);
 
-        rataRataNilaiPerMahasiswa(studentNames, scores);
+        rataRataNilaiPerMahasiswa(mahasiswa, scores);
 
-        rataRataNilaiPerTugas(jumlahTugas, studentNames, scores);
+        rataRataNilaiPerTugas(jumlahTugas, mahasiswa, scores);
 
         sc.close();
     }
 
-    public static void tampilkanNilaiMahasiswa(String[] studentNames, int[][] scores) {
+    public static void tampilkanNilaiMahasiswa(String[] mahasiswa, int[][] scores) {
         System.out.println("\nData Nilai Mahasiswa:");
-        for (int i = 0; i < studentNames.length; i++) {
-            System.out.print(studentNames[i] + ": ");
+        for (int i = 0; i < mahasiswa.length; i++) {
+            System.out.print(mahasiswa[i] + ": ");
             for (int j = 0; j < scores[i].length; j++) {
                 System.out.print(scores[i][j] + " ");
             }
@@ -45,26 +45,26 @@ public class ModifTugasNo2 {
         }
     }
 
-    public static void rataRataNilaiPerMahasiswa(String[] studentNames, int[][] scores) {
+    public static void rataRataNilaiPerMahasiswa(String[] mahasiswa, int[][] scores) {
         System.out.println("\nRata-rata Nilai Per Mahasiswa:");
-        for (int i = 0; i < studentNames.length; i++) {
+        for (int i = 0; i < mahasiswa.length; i++) {
             int total = 0;
             for (int j = 0; j < scores[i].length; j++) {
                 total += scores[i][j];
             }
             double rataRata = (double) total / scores[i].length;
-            System.out.println(studentNames[i] + ": " + rataRata);
+            System.out.println(mahasiswa[i] + ": " + rataRata);
         }
     }
 
-    public static void rataRataNilaiPerTugas(int jumlahTugas, String[] studentNames, int[][] scores) {
+    public static void rataRataNilaiPerTugas(int jumlahTugas, String[] mahasiswa, int[][] scores) {
         System.out.println("\nRata-rata Nilai Per Tugas:");
         for (int j = 0; j < jumlahTugas; j++) {
             int total = 0;
-            for (int i = 0; i < studentNames.length; i++) {
+            for (int i = 0; i < mahasiswa.length; i++) {
                 total += scores[i][j];
             }
-            double rataRata = (double) total / studentNames.length;
+            double rataRata = (double) total / mahasiswa.length;
             System.out.println("Tugas " + (j + 1) + ": " + rataRata);
         }
     }
